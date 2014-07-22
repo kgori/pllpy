d3e2298715c5d5af23feccc7f41030b0c0c91ec3
Kevin Gori <kgori@ebi.ac.uk>
2014-07-22 17:31:11 +0100

    Minor increment

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ ext = Extension("pllpy",
 
 setup(cmdclass={'build_ext':build_ext},
       name="pllpy",
-      version="0.1.1",
+      version="0.1.2",
       author='Kevin Gori',
       author_email='kgori@ebi.ac.uk',
       description='Wrapper for Phylogenetic Likelihood Library',

5da5f0cf42ad09436eda31e3b0820d66d36963ba
Kevin Gori <kgori@ebi.ac.uk>
2014-10-07 21:30:01 +0100

    Version 0.1.10 pushed to pypi

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ ext = Extension("pllpy",
 
 setup(cmdclass={'build_ext':build_ext},
       name="pllpy",
-      version="0.1.9",
+      version="0.1.10",
       author='Kevin Gori',
       author_email='kgori@ebi.ac.uk',
       description='Wrapper for Phylogenetic Likelihood Library',

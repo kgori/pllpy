1beba32002506d89b4b4bfae4263bc445aa0d60d
Kevin Gori <kgori@ebi.ac.uk>
2017-01-16 11:15:10 +0000

    Problem: Compilation failure on Mac. Changes: Removed autowrap dependency in pllpy.pyx, added platform-specific compiler flags in setup.py, removed using namespace std

--- setup.py
+++ setup.py
@@ -4,28 +4,46 @@ except ImportError:
     from distutils.core import setup, Extension
 
 from Cython.Distutils import build_ext
-import pkg_resources
 
-data_dir = pkg_resources.resource_filename("autowrap", "data_files")
+import os, platform, re, subprocess
+
+def is_clang(bin):
+    proc = subprocess.Popen([bin, '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    output = str(b'\n'.join([stdout, stderr]).decode('ascii', 'ignore'))
+    return not re.search(r'clang', output) is None
+
+class my_build_ext(build_ext):
+
+    def build_extensions(self):
+        binary = self.compiler.compiler[0]
+        if is_clang(binary):
+            for e in self.extensions:
+                e.extra_compile_args.append('-stdlib=libc++')
+                if platform.system() == 'Darwin':
+                    e.extra_compile_args.append('-mmacosx-version-min=10.7')
+                    e.extra_link_args.append('-mmacosx-version-min=10.7')
+
+        build_ext.build_extensions(self)
+
 
 ext = Extension("pllpy",
                 sources = ['src/pllpy.pyx', 'src/pllml.cpp'],
                 language="c++",
-                include_dirs = [data_dir],
+                include_dirs = [],
                 libraries=['pll-sse3-pthreads'],
                 extra_compile_args=['-std=c++11'],
                )
 
-setup(cmdclass={'build_ext':build_ext},
+setup(cmdclass={'build_ext':my_build_ext},
       name="pllpy",
-      version="0.1.13",
+      version="0.1.17",
       author='Kevin Gori',
       author_email='kgori@ebi.ac.uk',
       description='Wrapper for Phylogenetic Likelihood Library',
       url='https://github.com/kgori/pllpy.git',
       ext_modules = [ext],
       install_requires=[
-        'autowrap',
         'cython',
       ],
       scripts=['bin/pll']
--- src/pllpy.pyx
+++ src/pllpy.pyx
@@ -4,25 +4,22 @@ from  libcpp.set     cimport set as libcpp_set
 from  libcpp.vector  cimport vector as libcpp_vector
 from  libcpp.pair    cimport pair as libcpp_pair
 from  libcpp.map     cimport map  as libcpp_map
-from  smart_ptr cimport shared_ptr
-from  AutowrapRefHolder cimport AutowrapRefHolder
 from  libcpp cimport bool
 from  libc.string cimport const_char
 from cython.operator cimport dereference as deref, preincrement as inc, address as address
 from pllml cimport pll as _pll
-cdef extern from "autowrap_tools.hpp":
-    char * _cast_const_away(char *) 
+
 
 cdef class pll:
 
-    cdef shared_ptr[_pll] inst
+    cdef _pll *inst
 
     def __dealloc__(self):
-         self.inst.reset()
+         del self.inst
 
     
     def get_model_names(self):
-        _r = self.inst.get().get_model_names()
+        _r = self.inst.get_model_names()
         cdef list py_result = _r
         return py_result
     
@@ -31,50 +28,50 @@ cdef class pll:
         assert isinstance(optimisable, (int, long)), 'arg optimisable wrong type'
     
     
-        self.inst.get().set_optimisable_rates((<int>partition), (<bool>optimisable))
+        self.inst.set_optimisable_rates((<int>partition), (<bool>optimisable))
     
     def optimise_freqs(self):
-        self.inst.get().optimise_freqs()
+        self.inst.optimise_freqs()
     
     def optimise_alphas(self):
-        self.inst.get().optimise_alphas()
+        self.inst.optimise_alphas()
     
     def set_optimisable_alpha(self,  partition ,  optimisable ):
         assert isinstance(partition, (int, long)), 'arg partition wrong type'
         assert isinstance(optimisable, (int, long)), 'arg optimisable wrong type'
     
     
-        self.inst.get().set_optimisable_alpha((<int>partition), (<bool>optimisable))
+        self.inst.set_optimisable_alpha((<int>partition), (<bool>optimisable))
     
     def get_rates(self):
-        _r = self.inst.get().get_rates()
+        _r = self.inst.get_rates()
         cdef list py_result = _r
         return py_result
     
     def get_rates_vector(self,  partition ):
         assert isinstance(partition, (int, long)), 'arg partition wrong type'
     
-        _r = self.inst.get().get_rates_vector((<int>partition))
+        _r = self.inst.get_rates_vector((<int>partition))
         cdef list py_result = _r
         return py_result
     
     def is_optimisable_frequencies(self,  partition ):
         assert isinstance(partition, (int, long)), 'arg partition wrong type'
     
-        cdef bool _r = self.inst.get().is_optimisable_frequencies((<int>partition))
+        cdef bool _r = self.inst.is_optimisable_frequencies((<int>partition))
         py_result = <bool>_r
         return py_result
     
     def optimise_model(self):
-        self.inst.get().optimise_model()
+        self.inst.optimise_model()
     
     def link_alpha_parameters(self, bytes linkage ):
         assert isinstance(linkage, bytes), 'arg linkage wrong type'
     
-        self.inst.get().link_alpha_parameters((<libcpp_string>linkage))
+        self.inst.link_alpha_parameters((<libcpp_string>linkage))
     
     def get_number_of_threads(self):
-        cdef int _r = self.inst.get().get_number_of_threads()
+        cdef int _r = self.inst.get_number_of_threads()
         py_result = <int>_r
         return py_result
     
@@ -85,54 +82,54 @@ cdef class pll:
         cdef libcpp_vector[double] v0 = rates
     
     
-        self.inst.get().set_rates(v0, (<int>partition), (<bool>optimisable))
+        self.inst.set_rates(v0, (<int>partition), (<bool>optimisable))
         
     
     def set_tree(self, bytes nwk):
         assert isinstance(nwk, bytes), 'arg nwk wrong type'
 
-        self.inst.get().set_tree((<libcpp_string>nwk))
+        self.inst.set_tree((<libcpp_string>nwk))
 
 
     def get_model_name(self,  partition ):
         assert isinstance(partition, (int, long)), 'arg partition wrong type'
     
-        cdef libcpp_string _r = self.inst.get().get_model_name((<int>partition))
+        cdef libcpp_string _r = self.inst.get_model_name((<int>partition))
         py_result = <libcpp_string>_r
         return py_result
     
     def set_number_of_threads(self,  threads ):
         assert isinstance(threads, (int, long)), 'arg threads wrong type'
     
-        self.inst.get().set_number_of_threads((<int>threads))
+        self.inst.set_number_of_threads((<int>threads))
     
     def optimise_branch_lengths(self,  num_iter ):
         assert isinstance(num_iter, (int, long)), 'arg num_iter wrong type'
     
-        self.inst.get().optimise_branch_lengths((<int>num_iter))
+        self.inst.optimise_branch_lengths((<int>num_iter))
     
     def is_optimisable_rates(self,  partition ):
         assert isinstance(partition, (int, long)), 'arg partition wrong type'
     
-        cdef bool _r = self.inst.get().is_optimisable_rates((<int>partition))
+        cdef bool _r = self.inst.is_optimisable_rates((<int>partition))
         py_result = <bool>_r
         return py_result
     
     def set_epsilon(self, double epsilon ):
         assert isinstance(epsilon, float), 'arg epsilon wrong type'
     
-        self.inst.get().set_epsilon((<double>epsilon))
+        self.inst.set_epsilon((<double>epsilon))
     
     def link_rates(self, bytes linkage ):
         assert isinstance(linkage, bytes), 'arg linkage wrong type'
     
-        self.inst.get().link_rates((<libcpp_string>linkage))
+        self.inst.link_rates((<libcpp_string>linkage))
     
     def optimise_rates(self):
-        self.inst.get().optimise_rates()
+        self.inst.optimise_rates()
     
     def get_alphas(self):
-        _r = self.inst.get().get_alphas()
+        _r = self.inst.get_alphas()
         cdef list py_result = _r
         return py_result
     
@@ -143,13 +140,13 @@ cdef class pll:
         cdef libcpp_vector[double] v0 = freqs
     
     
-        self.inst.get().set_frequencies(v0, (<int>partition), (<bool>optimisable))
+        self.inst.set_frequencies(v0, (<int>partition), (<bool>optimisable))
         
     
     def is_protein(self,  partition ):
         assert isinstance(partition, (int, long)), 'arg partition wrong type'
     
-        cdef bool _r = self.inst.get().is_protein((<int>partition))
+        cdef bool _r = self.inst.is_protein((<int>partition))
         py_result = <bool>_r
         return py_result
     
@@ -158,34 +155,34 @@ cdef class pll:
         assert isinstance(optimisable, (int, long)), 'arg optimisable wrong type'
     
     
-        self.inst.get().set_optimisable_frequencies((<int>partition), (<bool>optimisable))
+        self.inst.set_optimisable_frequencies((<int>partition), (<bool>optimisable))
     
     def is_optimisable_alpha(self,  partition ):
         assert isinstance(partition, (int, long)), 'arg partition wrong type'
     
-        cdef bool _r = self.inst.get().is_optimisable_alpha((<int>partition))
+        cdef bool _r = self.inst.is_optimisable_alpha((<int>partition))
         py_result = <bool>_r
         return py_result
     
     def get_tree(self):
-        cdef libcpp_string _r = self.inst.get().get_tree()
+        cdef libcpp_string _r = self.inst.get_tree()
         py_result = <libcpp_string>_r
         return py_result
     
     def get_frequencies_vector(self,  partition ):
         assert isinstance(partition, (int, long)), 'arg partition wrong type'
     
-        _r = self.inst.get().get_frequencies_vector((<int>partition))
+        _r = self.inst.get_frequencies_vector((<int>partition))
         cdef list py_result = _r
         return py_result
     
     def get_number_of_partitions(self):
-        cdef int _r = self.inst.get().get_number_of_partitions()
+        cdef int _r = self.inst.get_number_of_partitions()
         py_result = <int>_r
         return py_result
     
     def get_epsilon(self):
-        cdef double _r = self.inst.get().get_epsilon()
+        cdef double _r = self.inst.get_epsilon()
         py_result = <double>_r
         return py_result
     
@@ -195,12 +192,7 @@ cdef class pll:
         assert isinstance(tree, bytes), 'arg tree wrong type'
         assert isinstance(num_threads, (int, long)), 'arg num_threads wrong type'
         assert isinstance(rns, (int, long)), 'arg rns wrong type'
-    
-    
-    
-    
-    
-        self.inst = shared_ptr[_pll](new _pll((<libcpp_string>alignment_file), (<libcpp_string>partitions), (<libcpp_string>tree), (<int>num_threads), (<long int>rns)))
+        self.inst = new _pll((<libcpp_string>alignment_file), (<libcpp_string>partitions), (<libcpp_string>tree), (<int>num_threads), (<long int>rns))
     
     def _init_1(self, bytes alignment_file , bytes partitions ,  parsimony ,  num_threads ,  rns ):
         assert isinstance(alignment_file, bytes), 'arg alignment_file wrong type'
@@ -208,12 +200,7 @@ cdef class pll:
         assert isinstance(parsimony, (int, long)), 'arg parsimony wrong type'
         assert isinstance(num_threads, (int, long)), 'arg num_threads wrong type'
         assert isinstance(rns, (int, long)), 'arg rns wrong type'
-    
-    
-    
-    
-    
-        self.inst = shared_ptr[_pll](new _pll((<libcpp_string>alignment_file), (<libcpp_string>partitions), (<bool>parsimony), (<int>num_threads), (<long int>rns)))
+        self.inst = new _pll((<libcpp_string>alignment_file), (<libcpp_string>partitions), (<bool>parsimony), (<int>num_threads), (<long int>rns))
     
     def __init__(self, *args):
         if (len(args)==5) and (isinstance(args[0], bytes)) and (isinstance(args[1], bytes)) and (isinstance(args[2], bytes)) and (isinstance(args[3], (int, long))) and (isinstance(args[4], (int, long))):
@@ -226,19 +213,19 @@ cdef class pll:
     def get_alpha(self,  partition ):
         assert isinstance(partition, (int, long)), 'arg partition wrong type'
     
-        cdef double _r = self.inst.get().get_alpha((<int>partition))
+        cdef double _r = self.inst.get_alpha((<int>partition))
         py_result = <double>_r
         return py_result
     
     def get_partition_names(self):
-        _r = self.inst.get().get_partition_names()
+        _r = self.inst.get_partition_names()
         cdef list py_result = _r
         return py_result
     
     def is_dna(self,  partition ):
         assert isinstance(partition, (int, long)), 'arg partition wrong type'
     
-        cdef bool _r = self.inst.get().is_dna((<int>partition))
+        cdef bool _r = self.inst.is_dna((<int>partition))
         py_result = <bool>_r
         return py_result
     
@@ -251,7 +238,7 @@ cdef class pll:
     
     
     
-        self.inst.get().optimise((<bool>rates), (<bool>freqs), (<bool>alphas), (<bool>branches))
+        self.inst.optimise((<bool>rates), (<bool>freqs), (<bool>alphas), (<bool>branches))
     
     def set_alpha(self, double alpha ,  partition ,  optimisable ):
         assert isinstance(alpha, float), 'arg alpha wrong type'
@@ -260,41 +247,41 @@ cdef class pll:
     
     
     
-        self.inst.get().set_alpha((<double>alpha), (<int>partition), (<bool>optimisable))
+        self.inst.set_alpha((<double>alpha), (<int>partition), (<bool>optimisable))
     
     def get_empirical_frequencies(self):
-        _r = self.inst.get().get_empirical_frequencies()
+        _r = self.inst.get_empirical_frequencies()
         cdef list py_result = _r
         return py_result
     
     def optimise_tree_search(self,  estimate_model ):
         assert isinstance(estimate_model, (int, long)), 'arg estimate_model wrong type'
     
-        self.inst.get().optimise_tree_search((<bool>estimate_model))
+        self.inst.optimise_tree_search((<bool>estimate_model))
     
     def get_partition_name(self,  partition ):
         assert isinstance(partition, (int, long)), 'arg partition wrong type'
     
-        cdef libcpp_string _r = self.inst.get().get_partition_name((<int>partition))
+        cdef libcpp_string _r = self.inst.get_partition_name((<int>partition))
         py_result = <libcpp_string>_r
         return py_result
     
     def get_frac_change(self):
-        cdef double _r = self.inst.get().get_frac_change()
+        cdef double _r = self.inst.get_frac_change()
         py_result = <double>_r
         return py_result
     
     def link_frequencies(self, bytes linkage ):
         assert isinstance(linkage, bytes), 'arg linkage wrong type'
     
-        self.inst.get().link_frequencies((<libcpp_string>linkage))
+        self.inst.link_frequencies((<libcpp_string>linkage))
     
     def get_likelihood(self):
-        cdef double _r = self.inst.get().get_likelihood()
+        cdef double _r = self.inst.get_likelihood()
         py_result = <double>_r
         return py_result
     
     def get_frequencies(self):
-        _r = self.inst.get().get_frequencies()
+        _r = self.inst.get_frequencies()
         cdef list py_result = _r
         return py_result 
